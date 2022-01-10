8ba450615b1a40c5d84bdb1e9c5753643a495f79
othmansh0 <othmanshahrouri@gmail.com>
2022-01-10 16:37:37 +0200

    Sms bottomSheet complete, constraints needs more testing

--- FirestoreDemo/Views/PhoneViewController.swift
+++ FirestoreDemo/Views/PhoneViewController.swift
@@ -129,3 +129,4 @@ class PhoneViewController: UIViewController, UITextFieldDelegate {
     
 }
 
+
--- FirestoreDemo/Views/SMSViewController.swift
+++ FirestoreDemo/Views/SMSViewController.swift
@@ -8,57 +8,58 @@
 import UIKit
 
 class SMSViewController: UIViewController, UISearchTextFieldDelegate {
-    @IBOutlet var codeField: UITextField!
-    @IBOutlet weak var codeTextField: CodeTextFieldViewController!
-    @IBOutlet weak var smsRectangle: UIImageView!
     var accountType:Int!
+    private var isBottomSheetShown = false
     
+    @IBOutlet weak var codeTextField: CodeTextFieldViewController!
+    @IBOutlet weak var bottomView: UIView!
+    
+    @IBOutlet weak var bottomViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomViewTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomViewLeadingConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.setBackgroundImage("greenBG", contentMode: .scaleAspectFit)
+        
         codeTextField.configure()
         codeTextField.didEnterLastDigit = { [weak self] code in
             print("code is\(code)")
             self?.checkSmsCode(check: code)
             //succeeded
         }
-     
-        print(accountType)
         codeTextField.delegate = self
+        
+        print(accountType)
+        
         let defaults = UserDefaults.standard
         defaults.setValue(accountType, forKey: "accountType")
         //codeTextField.configure()
+        
+        //MARK: bottomsheet design
+        bottomView.layer.cornerRadius = 30
+        bottomView.clipsToBounds = true
+        
+        showBottomSheet()
+        
+        //makes UIView clickable
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissBottomSheet))
+        view.addGestureRecognizer(tap)
     }
     
- 
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        stretchBottomSheet()
+    }
     
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//
-//        textField.resignFirstResponder()
-//        if let code = codeField.text {
-//            AuthManager.shared.verifyCode(smsCode: code) {[weak self] (success) in
-//                guard success else {
-//
-//                    return }
-//                if self?.accountType == 0 {
-//                    if let vc = self?.storyboard?.instantiateViewController(identifier: "Doctor") as? DoctorViewController {
-//                        self?.navigationController?.pushViewController(vc, animated: true)
-//                    }
-//                } else {
-//                    if let vc = self?.storyboard?.instantiateViewController(identifier: "Patient") as? PatientViewController {
-//                        self?.navigationController?.pushViewController(vc, animated: true)
-//                    }
-//                }
-//
-//
-//            }
-//        }
-//
-//        return true
-//    }
+    
+    
+}
 
+extension SMSViewController {
+    //checks sms code
     private func checkSmsCode(check code:String) {
-    
             AuthManager.shared.verifyCode(smsCode: code) {[weak self] (success) in
                 guard success else {
                   
@@ -72,11 +73,52 @@ class SMSViewController: UIViewController, UISearchTextFieldDelegate {
                         self?.navigationController?.pushViewController(vc, animated: true)
                     }
                 }
-              
                 
             }
+    }
+    
+    
+    //MARK: Bottom Sheet Animation (NEDINE)
+    public func showBottomSheet(){
+        if (isBottomSheetShown) {
+            //default Bottom sheet
+            UIView.animate(withDuration: 0.3) {
+                self.bottomViewHeightConstraint.constant = 400
+                // update view layout immediately
+                self.view.layoutIfNeeded()
+            } completion: { status in
+                self.isBottomSheetShown = false
+            }
+        }
         
     }
     
+    
+    public func stretchBottomSheet(){
+        UIView.animate(withDuration: 0.3) {
+            self.bottomViewHeightConstraint.constant = 620
+            // update view layout immediately
+            self.view.layoutIfNeeded()
+        } completion: { status in
+            self.isBottomSheetShown = true
+            
+            //bouncing animation when card is shown
+            UIView.animate(withDuration: 0.3) {
+                self.bottomViewHeightConstraint.constant = 600
+                
+                self.view.layoutIfNeeded()
+            } completion: { status in
+                
+            }
+        
+        }
+    }
+    
+    @objc func dismissBottomSheet() {
+        //return to min height
+        showBottomSheet()
+        //removes focus of phoneField
+        codeTextField.resignFirstResponder()
+    }
     
 }
