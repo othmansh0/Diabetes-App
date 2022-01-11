cabd21d07cb726e604b977550e0d85adf06bfd0d
othmansh0 <othmanshahrouri@gmail.com>
2022-01-11 16:15:36 +0200

    Complete registeration backend for Patient

--- FirestoreDemo/Views/PatientViewController.swift
+++ FirestoreDemo/Views/PatientViewController.swift
@@ -11,6 +11,10 @@ class PatientViewController: UIViewController {
     @IBOutlet var nameField: UITextField!
     @IBOutlet var ageField: UITextField!
     @IBOutlet var doctorID: UITextField!
+    @IBOutlet weak var diabetesType: UISegmentedControl!
+    @IBOutlet weak var patientHeight: UITextField!
+    @IBOutlet weak var patientWeight: UITextField!
+    
     let db = Firestore.firestore()
 
     override func viewDidLoad() {
@@ -21,8 +25,22 @@ class PatientViewController: UIViewController {
     
 
     @IBAction func submitPressed(_ sender: UIButton) {
-        let newPatient = db.collection("doctors").document(doctorID.text!).collection("patients").document(Auth.auth().currentUser!.uid)
-        newPatient.setData(["name":"WE DID IT","ID":newPatient.documentID]) 
+        
+        if let Patientname = nameField.text,let pateintAge = ageField.text, let doctorID  = doctorID.text, let patientHeight = patientHeight.text, let patientWeight = patientWeight.text,let diabetesType = diabetesType.titleForSegment(at: diabetesType.selectedSegmentIndex){
+            print(diabetesType)
+            
+            
+            
+            let newPatient = db.collection("doctors").document(doctorID).collection("patients").document(Auth.auth().currentUser!.uid)
+            newPatient.setData(["Name":Patientname,"Age":pateintAge,"Height":patientHeight,"Weight":patientWeight,"DiabetesType":diabetesType,"ID":newPatient.documentID])
+            
+            
+           
+            
+        }
+        
+        
+       
     }
     
 }
